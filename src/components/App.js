a475e618845349801ac5274684fb2a2debac5490
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-01-30 01:07:07 +0300

    Completed Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [image, setImage] = useState(null);
+
+    //fetch data using the useEffect url provided
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                setImage(data.message);
+            });
+    }, []);
+    
+    //JSX code
+    return (
+        <>
+            {image ? <img src={image} alt="A Random Dog" /> : <p>Loading...</p>}
+        </>
+    );
+
+}
+
+export default App;
